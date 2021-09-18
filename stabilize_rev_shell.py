e7a78820fd033bc64127abc2fbcacbf6418ed0ac
crackedskull <90918934+crackedskull@users.noreply.github.com>
2021-09-18 10:37:34 +0530

    Update stabilize_rev_shell.py

--- stabilize_rev_shell.py
+++ stabilize_rev_shell.py
@@ -1,41 +1,40 @@
 import pyautogui
 import time
 
-importing_pty = '"import pty;pty.spawn("bin/bash")"'
 
 def dumb_to_smart():
-    usr_in = input("[!] Please check if python is installed on the target server using [which python] (y/n): ")
-    port = input("Port on which the dumb reverse shell is running on: ")
     lines = input("No. of lines your terminal has: ") #Command to determine number of lines = tput lines
     columns = input("No. of columns your terminal has: ") #Command to determine number of columns = tput cols
-    if usr_in == "y":
-        print("Intialzing Script.........")
-    else:
-        print("Script aborted")
-        exit()
-    time.sleep(2)
-    pyautogui.write(f"python -c {importing_pty}")
+
+    print("[!] Take your cursor to the terminal in which the nc session is running")
+
+    time.sleep(3)
+
+    print("Initializing Script...")
+
+    pyautogui.write("/usr/bin/script -qc /bin/bash /dev/null")
     pyautogui.press("enter")
     time.sleep(1)
-    with pyautogui.hold('ctrl'):
+
+    with pyautogui.hold("ctrl"):
         pyautogui.press("z")
-    time.sleep(1)
-    pyautogui.write("stty -raw echo")
+
+    pyautogui.write("stty raw -echo")
     pyautogui.press("enter")
     time.sleep(1)
+
     pyautogui.write("fg")
     pyautogui.press("enter")
-    time.sleep(1)
-    pyautogui.write("reset")
     pyautogui.press("enter")
     time.sleep(1)
-    pyautogui.write("exterm")
+
+    pyautogui.write("export TERM=xterm")
     pyautogui.press("enter")
     time.sleep(1)
-    pyautogui.write(f"stty rows {lines} columns {columns}")
+
+    pyautogui.write(f"stty cols {columns} rows {lines}")
     pyautogui.press("enter")
-    time.sleep(1)
-    print("Shell stabilized!")
+
     exit()
 
 dumb_to_smart()
